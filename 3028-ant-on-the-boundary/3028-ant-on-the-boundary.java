0bbaeef54e8101a65ab7fdb2469e330e8f867ab3
ShaikhInayat1 <shaikhinayat.cs@gmail.com>
2026-07-05 19:59:49 +0530

    Time: 0 ms (100%), Space: 44.1 MB (27.16%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        
+        int sum = 0;
+        int count = 0;
+
+        for(int i : nums){
+
+            sum += i;
+
+            if(sum == 0){
+                
+                count ++;
+            }
+        }
+
+            return count;
+    }
+}
