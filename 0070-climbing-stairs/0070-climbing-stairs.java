48cbf82593762598f4894a27cb3da59ddb0360d2
ShaikhInayat1 <shaikhinayat.cs@gmail.com>
2026-07-17 15:25:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.07%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n <= 2){
+
+            return n;
+        }
+
+        int step1 = 1;
+        int step2 = 2;
+
+        for(int i = 3; i <= n; i++){
+
+            int step3 = step1 + step2;
+            step1 = step2;
+            step2 = step3;
+        }
+
+        return step2;
+    }
+}
